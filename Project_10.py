c76cba7e69cb05e6f5e9215a4ed6a6116b328975
Mahdi <123685546+MahdiUK@users.noreply.github.com>
2023-01-29 19:04:56 +0000

    Add files via upload

---
+++ Project_10.py
@@ -0,0 +1,12 @@
+A = int (input(' Please Enter the number of A: '))
+B = int (input(' Please Enter the number of B: '))
+C = int (input(' Please Enter the number of C: '))
+D = int (input(' Please Enter the number of D: '))
+E = int (input(' Please Enter the number of E: '))
+
+SUM = A + B + C + D + E
+AVERAGE = (A + B + C + D + E) / 5
+
+
+print(f'Sum of Numbers: {SUM}')
+print (f'Average of Numbers: {AVERAGE}')
